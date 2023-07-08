617c64025084df5804c90e45fa2a504237ff29ca
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2023-07-08 03:03:56 +0300

    feat: begin
    trash

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,14 @@
+import { BuildOptions } from "./types/config";
+import type { Configuration as DevServerConfigurations } from 'webpack-dev-server'
+
+export function buildDevServer(options: BuildOptions): DevServerConfigurations {
+    return {
+        port: options.port,
+        open: true,
+        /**
+           * Исправляет работу роутинга
+           */
+        historyApiFallback: true,
+        hot: true
+    }
+}
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+
+const AppRouter = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                {Object.values(routeConfig).map(({ element, path }) => (
+                    <Route
+                        path={path}
+                        key={path}
+                        element={(
+                            <div className='page-wrapper'>
+                                {element}
+                            </div>
+                        )}
+                    />
+                ))}
+            </Routes>
+        </Suspense>
+    )
+}
+
+export default AppRouter
