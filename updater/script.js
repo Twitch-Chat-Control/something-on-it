e6e0e1502d5177a9ad12d04c3ed91565c77f38fc
Vladislav Laptyonok <v.laptyonok@itransition.com>
2023-07-08 10:32:53 +0300

    fix(ci): try to check env vars

--- updater/script.js
+++ updater/script.js
@@ -5,11 +5,13 @@ import {XMLParser} from "fast-xml-parser";
 const options = {ignoreAttributes: false};
 const parser = new XMLParser(options);
 
-axios
-    .get(process.env.PODCAST_RSS_URL)
-    .then(value => {
-        parseRss(value.data)
-    });
+console.log(process.env);
+
+// axios
+//     .get(process.env.PODCAST_RSS_URL)
+//     .then(value => {
+//         parseRss(value.data)
+//     });
 
 function parseRss(rss) {
     let response = parser.parse(rss).rss;
